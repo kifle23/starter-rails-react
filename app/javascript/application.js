df35d2ca1bd8d25e018ad4626b90698488deb657
kifle23 <kiflekifle23@gmail.com>
2023-09-19 23:32:20 +0300

    Update application js

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,5 @@
-// Entry point for the build script in your package.json
-import "@hotwired/turbo-rails"
-import "./controllers"
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App';
+
+ReactDOM.render(<App />, document.getElementById('root'));
