a10cb36ecbe284aee6533ac0deda5126d71b03db
kifle23 <kiflekifle23@gmail.com>
2023-09-20 06:48:34 +0300

    Update render method to create root

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom';
 import App from './components/App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+
+createRoot(document.getElementById('root')).render(<App />);
